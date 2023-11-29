75257b8baa94c17b95ca4daac4e96b65cf0a3d92
hzmsalah <hzmsalah2@gmail.com>
2023-11-29 12:10:46 +0200

    Variables for ResultRanker class

--- src/main/java/oxCator/base/ResultRanker.java
+++ src/main/java/oxCator/base/ResultRanker.java
@@ -1,4 +1,9 @@
 package oxCator.base;
 
+import java.util.*;
+
 public class ResultRanker {
+    private HashMap<String, PositionalIndex> positionalIndex; //->df & M
+    private HashMap<String, Integer> termCount; //->Tf
+    public HashMap<Integer, String> documentList; //-> N
 }
