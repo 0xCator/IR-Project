5a97f6723d964f28e20cee3378c5be3542b8c2d0
hzmsalah <hzmsalah2@gmail.com>
2023-11-29 12:12:04 +0200

    Testing commits through IntelliJ

--- src/main/java/oxCator/base/ResultRanker.java
+++ src/main/java/oxCator/base/ResultRanker.java
@@ -6,4 +6,5 @@ public class ResultRanker {
     private HashMap<String, PositionalIndex> positionalIndex; //->df & M
     private HashMap<String, Integer> termCount; //->Tf
     public HashMap<Integer, String> documentList; //-> N
+    public int x;
 }
